4fdb5f4a40debbf3e2c698c9476746899735375f
Fazeel Khalid <fazeelkhalid9@gmail.com>
2024-02-04 15:02:33 +0500

    update ScanReport path

--- app/Models/Devices.php
+++ app/Models/Devices.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DB;
-use ScanReport;
+use App\Models\ScanReport;
 
 class Devices extends Model
 {
--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,7 +3,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use ScanReport;
+use App\Models\ScanReport;
 
 use App\Http\Controllers\GenerateIDController;
 
